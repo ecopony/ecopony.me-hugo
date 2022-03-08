bcc59c14159baaaf48950922fda29f4a9bdc7e51
Ed Copony <ecopony@gmail.com>
2022-03-07 19:11:32 -0800

    Add Pelican site

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+AUTHOR = 'Ed Copony'
+SITENAME = 'Ed Copony'
+SITEURL = ''
+
+THEME = "./pelican-themes/gum"
+
+DISPLAY_PAGES_ON_MENU = True
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/ecopony'),
+          ('LinkedIn', 'https://www.linkedin.com/in/ecopony/'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
